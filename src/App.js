4338e3b9ae705e75035f1d3e566ff5a62cdc0712
I-A11 <d.islam1983@gmail.com>
2021-03-23 16:18:28 +1100

    change state when click on checkbox

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ class App extends Component {
     this.state = {
       todos: todosData
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return updatedTodos;
+    });
+
+  }
+
   render() {
-    const todoItems = this.state.todos.map(item => <Todoitem key={item.id} item={item} />);
+    const todoItems = this.state.todos.map(item => <Todoitem key={item.id} item={item} handleChange={this.handleChange} />);
 
     return (
 
--- src/Todoitem.js
+++ src/Todoitem.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Todoitem = (props) => {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} />
+            <input
+                type="checkbox" checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
+            />
             <p>{props.item.text}</p>
 
         </div>
